35e11ed57de96fecf54b6091507e4cf45f275418
pricelessnothing <edaniile@mail.ru>
2020-03-25 18:48:02 +0300

    add draggable blocks

--- robbot/static/js/src/robbot.js
+++ robbot/static/js/src/robbot.js
@@ -4,14 +4,7 @@
 
 //eslint-disable-next-line no-unused-vars
 function RobbotXBlock(runtime, element) {
-    var handlerUrl = runtime.handlerUrl(element, 'test')
-
-    $.ajax({
-        type: 'POST',
-        url: handlerUrl,
-        data: JSON.stringify({'hello': 'world'}),
-        success: data => alert(data)
-    })
+    // var handlerUrl = runtime.handlerUrl(element, 'test')
 
     $(function() {
 
@@ -24,7 +17,8 @@ function RobbotXBlock(runtime, element) {
         const robbot = {
             x: 100,
             y: 100,
-            rot: 0,
+            rot: 90,
+            speed: 100,
             WL: 50,
             WR: 0,
             width: 50,
@@ -39,22 +33,48 @@ function RobbotXBlock(runtime, element) {
             }
         }
 
+        const workbench = {
+            blocks: [
+                {
+                    id: 1,
+                    type: 'start',
+                    text: '',
+                    inputs: [],
+                    outputs: [2],
+                    x: 50,
+                    y: 50,
+                },
+                {
+                    id: 2,
+                    type: 'instructions',
+                    text: 'x = 0',
+                    inputs: [1],
+                    outputs: [],
+                    x: 200,
+                    y: 50
+                }
+            ]
+        }
+
         let intervalID = null
+
+        let draggableBlock = null
 
         /* UI ELEMENTS DECLARATION */
 
-        const UIRobbot = document.getElementById('robbot')
-        const UIInfo = document.querySelector('#tab-content .run .toolbar .info')
-        const UIBtnPlayPause = document.querySelector('#tab-content .run .controls .play_pause')
-        const UIBtnNextStep = document.querySelector('#tab-content .run .controls .next_step')
-        const UITabs = document.querySelectorAll('#tab-header .tab-header-item')
-        const UIContents = document.querySelectorAll('#tab-content .tab-content-item')
-        const UIRun_ToolbarToggler = document.querySelector('#tab-content .run .toolbar .toolbar-toggler')
-        // const UIFinishSign = document.querySelectorAll('#tab-content .run .runtime .finish-sign')
+        const UIRobbot = $('robbot', element)
+        const UIInfo = $('#tab-content .run .toolbar .info', element)
+        const UIBtnPlayPause = $('#tab-content .run .controls .play_pause', element)
+        const UIBtnNextStep = $('#tab-content .run .controls .next_step', element)
+        const UITabs = $('#tab-header .tab-header-item', element)
+        const UIContents = $('#tab-content .tab-content-item', element)
+        const UIRun_ToolbarToggler = $('#tab-content .run .toolbar .toolbar-toggler', element)
+        const UISource_ToolbarToggler = $('#tab-content .source .toolbar .toolbar-toggler', element)
+        const UIWorkbench = $('#tab-content .source #workbench', element)
 
         /* EVENT LISTENERS */
 
-        UIBtnPlayPause.addEventListener('click', () => {
+        UIBtnPlayPause.on('click', () => {
             if (UIBtnPlayPause.classList.toggle('running')) {
                 executionRun()
             } else {
@@ -62,24 +82,30 @@ function RobbotXBlock(runtime, element) {
             }
         })
 
-        UIBtnNextStep.addEventListener('click', () => {
+        UIBtnNextStep.on('click', () => {
             executionPause()
             executionCycle()
         })
 
-        UIRun_ToolbarToggler.addEventListener('click', e => {
+        UIRun_ToolbarToggler.on('click', e => {
             e.target.parentNode.classList.toggle('closed')
             e.target.classList.toggle('closed')
             e.target.innerHTML = 'X' === e.target.innerHTML ? '<' : 'X'
         })
 
-        UITabs.forEach(tab => {
-            tab.addEventListener('click', e => {
-                UITabs.forEach(el => el.classList.remove('active'))
-                UIContents.forEach(el => el.classList.remove('active'))
-                const selectedContent = document.querySelector(`#tab-content .${e.target.getAttribute('data-toggle')}`)
-                selectedContent.classList.add('active')
-                e.target.classList.add('active')
+        UISource_ToolbarToggler.on('click', e => {
+            e.target.parentNode.classList.toggle('closed')
+            e.target.classList.toggle('closed')
+            e.target.innerHTML = 'X' === e.target.innerHTML ? '<' : 'X'
+        })
+
+        UITabs.each((i, tab) => {
+            $(tab).on('click', e => {
+                UITabs.each((i, el) => $(el).removeClass('active'))
+                UIContents.each((i, el) => $(el).removeClass('active'))
+                const selectedContent = $(`#tab-content .${e.target.getAttribute('data-toggle')}`)
+                selectedContent.addClass('active')
+                $(e.target).addClass('active')
             })
         })
 
@@ -96,18 +122,46 @@ function RobbotXBlock(runtime, element) {
             UIBtnPlayPause.innerHTML = '|>'
         }
 
+        const dragBlock = e => {
+            e = e || window.event
+            e.preventDefault()
+            if (draggableBlock) {
+                const x = draggableBlock.data('blockDraggingX') - e.clientX
+                const y = draggableBlock.data('blockDraggingY') - e.clientY
+                draggableBlock.data({
+                    blockDraggingX: e.clientX,
+                    blockDraggingY: e.clientY
+                })
+                draggableBlock.css({
+                    top: `${(draggableBlock[0].offsetTop - y)}px`,
+                    left: `${(draggableBlock[0].offsetLeft - x)}px`
+                })
+            }
+        }
+
+        const undragBlock = () => {
+            $(document).off('mousemove')
+            $(document).off('mouseup')
+            const blockToUpdate = workbench.blocks.filter(b => b.id === draggableBlock.data('block').id)[0]
+            console.log(blockToUpdate)
+            blockToUpdate.x = draggableBlock[0].offsetLeft
+            blockToUpdate.y = draggableBlock[0].offsetTop
+            draggableBlock = null
+            console.log(workbench)
+        }
         /* SOME BUSINESS LOGIC */
 
         function executionCycle() {
-            move()
-            render()
+            moveRobbot()
+            renderRuntime()
             if (Math.abs(robbot.x - map.finish.x) < 10 && Math.abs(robbot.y - map.finish.y) < 10) {
                 executionPause()
                 alert('You win!')
             }
         }
 
-        function move() {
+        //eslint-disable-next-line no-unused-vars
+        function move2motors() {
             let dS //distance covered per time quant
             if (robbot.WL === robbot.WR) {
                 dS = robbot.WL / cfg.fps
@@ -127,13 +181,49 @@ function RobbotXBlock(runtime, element) {
             robbot.x += dX
         }
 
-        function render() {
-            UIRobbot.style.left = `${robbot.x - 19.5}px`
-            UIRobbot.style.top = `${robbot.y - robbot.width / 2}px`
-            UIRobbot.style.transform = `rotate(${robbot.rot}rad)`
-            UIInfo.innerHTML = `LW: ${robbot.WL.toFixed(5)} <br> RW: ${robbot.WR.toFixed(5)} <br>
-                                Rotation: ${robbot.rot.toFixed(5)} rad <br>
-                                X: ${robbot.x.toFixed(5)} <br> Y: ${robbot.y.toFixed(5)}`
+        function moveRobbot() {
+            const {rot, speed} = robbot
+            const dS = speed / cfg.fps
+            robbot.y += Math.sin(rot / 180 * Math.PI) * dS
+            robbot.x += Math.cos(rot / 180 * Math.PI) * dS
         }
+
+        function renderRuntime() {
+            const {x, y, width, rot, speed} = robbot
+            UIRobbot.style.left = `${x - 19.5}px`
+            UIRobbot.style.top = `${y - width / 2}px`
+            UIRobbot.style.transform = `rotate(${rot}deg)`
+            UIInfo.innerHTML = `Speed: ${speed} <br>
+                                Rotation: ${rot.toFixed(5)} deg <br>
+                                X: ${x.toFixed(5)} <br> Y: ${y.toFixed(5)}`
+        }
+
+        function renderWorkbench() {
+            const { blocks } = workbench
+            UIWorkbench.empty()
+            blocks.forEach( block => {
+                $(`<div class="block block-${block.type}" id="block-${block.id}"></div>`)
+                    .data('block', block)
+                    .css({
+                        left: `${block.x}px`,
+                        top: `${block.y}px`
+                    })
+                    .appendTo(UIWorkbench)
+                    .on('mousedown', e => {
+                        e = e || window.event
+                        e.preventDefault()
+                        draggableBlock = $(e.target)
+                        $(e.target)
+                            .data({
+                                blockDraggingX: e.clientX,
+                                blockDraggingY: e.clientY
+                            })
+                        $(document).on('mousemove', dragBlock)
+                        $(document).on('mouseup', undragBlock)
+                    })
+            })
+        }
+
+        renderWorkbench()
     })
 }
