4190e29a8eb2b2ecff97ffdde530f17d0af5b61a
pricelessnothing <edaniile@mail.ru>
2020-03-25 18:48:44 +0300

    updated version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def package_data(pkg, roots):
 
 setup(
     name='robbot-xblock',
-    version='0.1',
+    version='0.2',
     description='robbot XBlock',   # TODO: write a better description.
     license='UNKNOWN',          # TODO: choose a license: 'AGPL v3' and 'Apache 2.0' are popular.
     packages=[
